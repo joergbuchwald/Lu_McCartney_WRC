29a3d6d241c8966d1a603c60edd43cc4dee1f6be
Jörg Buchwald <joerg.buchwald@ufz.de>
2025-11-28 17:16:03 +0100

    draft implementations

--- Lu_McCartney.py
+++ Lu_McCartney.py
@@ -38,6 +38,7 @@ class LuMcCartney:
         # material properties
         self.phi = 0.45 #m3/m3
         self.theta_s = self.phi #m3/m3
+        self.params = {}
     
     def water_content(self, psi, T):
         theta_ad = self.adsorptive_water_content(psi, T)
@@ -45,48 +46,57 @@ class LuMcCartney:
         return theta_ad + theta_cap
 
     def adsorptive_water_content(self, psi, T):
-        theta_ad = self.theta_a_max(T) * (1 - exp((psi-self.psi_max(T))/psi)**self.M)
-        return theta_ad
+        arg = (psi - self.psi_max(T))/psi
+        term = np.exp(arg)
+        return theta_a_max(T) * (1-term**M)
 
     def capillary_water_content(self, psi, T):
-        theta_cap = (self.theta_s - self.adsorptive_water_content(psi, T))/2 *(1 - erf(np.sqrt(2)*psi/self.phi_c(T)*((self.chi(T)+T)/(chi_r+Tr))))
+        theta_mean = (self.theta_s - self.adsorptive_water_content(psi, T))/2 
+        chi_frac = (self.chi(T) + T) / (self.chi_r + Tr)
+        erf_term = erf(np.sqrt(2)* (psi/self.phi_c(T)*chi_frac - 1))
+        power_term = (self.alpha(T) * psi * chi_frac)**self.N
+        cap_term = theta_mean * (1 - erf_term) * (1 + power_term)**(1/self.N-1)
         return theta_cap
 
     def theta_a_max(self, T):
-        #eq. 6
-        pass
+        return (1-theta_s) * (self.CEC(T) / self.zeta_s + self.bw)
+
     
-    def psi_max(self, T):
-        #eq. 4
-        pass
+    def psi_max(self, T, c=None):
+        umax = R_gas * T * self.c / (3000.0 * v_w)
+        return umax
     
     def c(self):
-        # eq. 5
-        pass
+        if "c" in self.params
+            return self.params['c']
+        else:
+            return np.exp((self.E1_minus_EL) / (R_gas * T))
+
 
     def CEC(self, T):
-        # eq. 8
-        pass
+        cos_arg = self.A * np.pi * (self.B*T - self.T1) / (self.T2 - self.T1)
+        return self.CEC_max * (np.cos(cos_arg) + 1) 
 
     def psi_c(self, T):
-        # eq. 25
-        pass
+        return self.A_H/(6*np.pi) * (self.theta_a_max(T) / (self.SSA * self.rho_d))**(-3)
 
     def A_H(self):
-        # eq23
-        pass
+        dielectric_term = (self.epsilon_s - self.epsilon_w)/(self.epsilon_s + self.epsilon_w)**2
+        pre_factor1 = (3 * self.kB * T)/4
+        pre_factor2 = 3 * (self.hp * self.nu_w)/(16 * np.sqrt(2))
+        RI_term = ((self.n_s**2 - self.n_w**2)**2/(self.n_s**2 + self.n_w**2)**(3/2))
+        return pre_factor1 * dielectric_term + pre_factor2 * RI_term
 
     def chi(self, T):
-        # eq. 18
-        pass
+        return -self.delta_h(T)/self.C_1
 
-    def delta_h(self):
-        # eq 13
-        pass
+    def delta_h(sel, T):
+        delta_hr = -0.516 # J/m^2 high plasticity clay
+        return delta_hr * ((1 - Tr)/(1 - T))**0.38
 
     def alpha(self):
-        # eq. 26
-        pass
+        psi_aev = self.alpha_0_inv * exp(self.eta_alpha * (Tr/T - 1))
+        return 1.0 / psi_aev
 
 
     
